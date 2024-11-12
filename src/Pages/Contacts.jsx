d9384460f0123c33a32e415e456bce5d1188b91f
Pedro Tello <Tello.3810@outlook.com>
2024-11-12 15:05:58 -0600

    Contacts update

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -4,8 +4,8 @@ import Layout from "./Layout";
 const Contacts = ({ children }) => {
   return (
     <Layout>
+      <h1 className="text-[2.5rem] text-white">CONTACTOS</h1>
       {children}
-      <h1>Contacts</h1>
     </Layout>
   );
 };
